b9bc9e3fbd3b2b6f81ff61bf32eef9fa2f4a5403
Conrad Kennington <conradkennington@crk.lan>
2022-06-04 16:49:31 -0600

    add json

---
+++ bom/random.js
@@ -0,0 +1,8 @@
+let url = 'https://ckennington.github.io/bom/bom.json';
+
+fetch(url)
+.then(res => res.json())
+.then((out) => {
+  console.log('Checkout this JSON! ', out);
+})
+.catch(err => { throw err });
